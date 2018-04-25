cc1b84ec3f3cdbfcb425ee2626c4da16bf84db54
christophjason <christopherjason98@gmail.com>
2018-04-25 18:36:36 +0800

    first commit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,57 @@
+<!-- edit.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laravel 5.6 CRUD Tutorial With Example </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+<div class="container">
+    <h2>Edit A Form</h2><br  />
+    <form method="post" action="{{action('PassportController@update', $id)}}">
+        @csrf
+        <input name="_method" type="hidden" value="PATCH">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" name="name" value="{{$passport->name}}">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" name="email" value="{{$passport->email}}">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <label for="number">Phone Number:</label>
+                <input type="text" class="form-control" name="number" value="{{$passport->number}}">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4" style="margin-left:38px">
+                <lable>Passport Office</lable>
+                <select name="office">
+                    <option value="Jakarta"  @if($passport->office=="Jakarta") selected @endif>Jakarta</option>
+                    <option value="Bekasi"  @if($passport->office=="Bekasi") selected @endif>Bekasi</option>
+                    <option value="Depok" @if($passport->office=="Depok") selected @endif>Depok</option>
+                    <option value="Tangerang" @if($passport->office=="Tangerang") selected @endif>Tangerang</option>
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4" style="margin-top:60px">
+                <button type="submit" class="btn btn-success" style="margin-left:38px">Update</button>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
